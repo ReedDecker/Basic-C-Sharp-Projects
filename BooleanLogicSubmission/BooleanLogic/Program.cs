d0d49bdd8022f8cae1bd076d851fbda9c541dff4
ReedDecker <reeddecker1@gmail.com>
2025-04-07 15:07:40 -0600

    Commit BooleanLogicSubmission
    Commit BooleanLogicSubmission to Basic-C-Sharp-Projects

---
+++ BooleanLogicSubmission/BooleanLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Boolean_Logic_Submission
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age;
+            int tickets;
+
+            bool DUI;
+            bool qualified;
+
+            //get input from user
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please type true or false");
+            DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            tickets = Convert.ToInt32(Console.ReadLine());
+
+            //determine qualification
+            qualified = ((age > 15) && (DUI == false) && (tickets <= 3));
+            Console.WriteLine("Qualified? \n" + qualified);
+            Console.ReadLine();
+        }
+    }
+}
+
