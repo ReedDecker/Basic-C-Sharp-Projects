49312eaa6313c1bb31c70da17588d47411e038a9
ReedDecker <reeddecker1@gmail.com>
2025-04-07 16:13:43 -0600

    Commit StringSubmission
    Commit String Submission to Basic-C-Sharp-Projects repo

---
+++ StringSubmission/StringSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+      
+        string str1 = "Goodbye";
+        string str2 = "Earthlings";
+        string str3 = "!";
+        string concatenatedString = str1 + " " + str2 + str3;
+        Console.WriteLine("Concatenated String: " + concatenatedString);
+
+     
+        string originalString = "This isn't my library voice.";
+        string upperCaseString = originalString.ToUpper();
+        Console.WriteLine("Uppercase String: " + upperCaseString);
+
+       
+        StringBuilder paragraph = new StringBuilder();
+        paragraph.AppendLine("This is the first sentence.");
+        paragraph.AppendLine("This is the second sentence.");
+        paragraph.AppendLine("This is the third sentence.");
+        paragraph.AppendLine("This is the fourth sentence.");
+
+        Console.WriteLine("\nParagraph: ");
+        Console.WriteLine(paragraph.ToString());
+
+        
+        Console.ReadKey();
+    }
+}
