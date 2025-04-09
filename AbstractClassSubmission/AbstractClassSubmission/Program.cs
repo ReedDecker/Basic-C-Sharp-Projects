f88b3427e255e6551c83c930ba34b967da7e4665
ReedDecker <reeddecker1@gmail.com>
2025-04-09 12:37:01 -0600

    Commit AbstractClassSubmission
    Commit AbstractClassSubmission to Basic C-Sharp repo

---
+++ AbstractClassSubmission/AbstractClassSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Create an abstract class called Person
+public abstract class Person
+{
+    // Define two properties: firstName and lastName
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Declare an abstract method SayName(). This method must be implemented by any class that inherits from Person.
+    public abstract void SayName();
+}
+
+// Create a class called Employee that inherits from Person
+public class Employee : Person
+{
+    // Implement the SayName method inside the Employee class
+    public override void SayName()
+    {
+        // Print the full name (FirstName + LastName) of the employee to the console
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate an Employee object with firstName "Sample" and lastName "Student"
+        Employee employee = new Employee()
+        {
+            FirstName = "Sample",
+            LastName = "Student"
+        };
+
+        // Call the SayName method on the employee object, which will print the name
+        employee.SayName();
+    }
+}
